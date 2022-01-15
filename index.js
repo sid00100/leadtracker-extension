4dbbee993693386b552bf1cc5e5496d73ebe8371
siddhant ota <otasiddhant@gmail.com>
2022-01-15 22:44:53 +0530

    Initial commit
    this is my first initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let myLeadJs = [];
+let textInputBoxJs = document.getElementById("text-input-box");
+let saveButtonJs = document.getElementById("save-btn");
+let leadListJs = document.getElementById("list-of-leads");
+
+saveButtonJs.addEventListener("click", function () {
+    leadListJs.innerHTML += `
+        <li>  <a target="_blank" href="${textInputBoxJs.value}"> ${textInputBoxJs.value} </a>  </li>
+    `
+    myLeadJs.push(textInputBoxJs.value)
+    myLeadJs = JSON.stringify(myLeadJs);
+
+    localStorage.setItem("lead", myLeadJs);
+    myLeadJs = JSON.parse(myLeadJs);
+    textInputBoxJs.value = "";
+    console.log(myLeadJs);
+
+})
